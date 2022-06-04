b13913925032ea207ada39403be86be5ebe73d02
Mafciejewicz <melonik0.9846@gmail.com>
2022-06-04 20:54:48 +0200

    Add navigation bar

---
+++ packages/mobile-app/src/Components/Navigation/Navigation.styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components/native';
+
+export const Wrapper = styled.View`
+    height: 80px;
+    flex-flow: row nowrap;
+    background: white;
+`;
+
+export const IconWrapper = styled.TouchableOpacity`
+    padding: 14px 10px;
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const Text = styled.Text<{ focused?: boolean }>`
+    color: ${({ focused, theme }) => (focused ? theme.palette.primaryAccent : '#C6C6C6')};
+    font-size: 11px;
+    margin-top: 5px;
+`;
---
+++ packages/mobile-app/src/Components/Navigation/Navigation.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { BottomTabBarProps } from '@react-navigation/bottom-tabs';
+import { IconWrapper, Wrapper, Text } from './Navigation.styles';
+import { ViewIconProps } from 'recipebase/src/views/HomeView/createViewIcon';
+
+export const Navigation: React.FC<BottomTabBarProps> = ({ state, descriptors, navigation }) => (
+    <Wrapper>
+        {state.routes.map((route, index) => {
+            const { options } = descriptors[route.key];
+            const label =
+                    options.tabBarLabel !== undefined
+                        ? options.tabBarLabel
+                        : options.title !== undefined
+                            ? options.title
+                            : route.name;
+
+            const Icon = options.tabBarIcon as React.FC<ViewIconProps>;
+            const isFocused = state.index === index;
+
+            const onPress = () => {
+                const event = navigation.emit({
+                    type: 'tabPress',
+                    target: route.key,
+                    canPreventDefault: true,
+                });
+
+                if (!isFocused && !event.defaultPrevented) {
+                    // The `merge: true` option makes sure that the params inside the tab screen are preserved
+                    // copied from the docs
+                    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                    // @ts-ignore
+                    navigation.navigate({ name: route.name, merge: true });
+                }
+            };
+
+            const onLongPress = () => {
+                navigation.emit({
+                    type: 'tabLongPress',
+                    target: route.key,
+                });
+            };
+
+            return (
+                <IconWrapper
+                    key={route.key}
+                    accessibilityRole="button"
+                    accessibilityState={isFocused ? { selected: true } : {}}
+                    accessibilityLabel={options.tabBarAccessibilityLabel}
+                    testID={options.tabBarTestID}
+                    onPress={onPress}
+                    onLongPress={onLongPress}
+                >
+                    <Icon focused={isFocused} />
+                    <Text focused={isFocused}>
+                        {label}
+                    </Text>
+                </IconWrapper>
+            );
+        })}
+    </Wrapper >
+);
